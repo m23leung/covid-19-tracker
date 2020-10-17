08cc24865b09cd7d5d75888287cfa4a50e52fb46
Mark Leung <mark.leung89@gmail.com>
2020-10-18 08:31:37 +1100

    Added InfoBox

---
+++ src/InfoBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+
+function InfoBox({ title, cases, total }) {
+    return (
+        <Card>
+            <CardContent>
+            
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
